0f6b37fdafecd45a77dfc15c16095c65dd968ba8
Žiga <ziga.sajovic@gmail.com>
2025-10-22 01:58:41 +0200

    fixed but in generate_offset_blocks

--- include/trueform/core/algorithm/generate_offset_blocks.hpp
+++ include/trueform/core/algorithm/generate_offset_blocks.hpp
@@ -4,12 +4,14 @@
  * https://github.com/xlabmedical/trueform
  */
 #pragma once
+#include "../blocked_buffer.hpp"
 #include "../offset_block_buffer.hpp"
 #include "./block_reduce_sequenced_aggregate.hpp"
 namespace tf {
-template <typename Range, typename Index, typename T, typename F>
+namespace core {
+template <typename Range, typename Index, typename Buffer, typename F>
 auto generate_offset_blocks(
-    const Range &input_data, tf::buffer<Index> &offsets, tf::buffer<T> &data,
+    const Range &input_data, tf::buffer<Index> &offsets, Buffer &data,
     const F &fill_block_f,
     std::size_t n_tasks = std::thread::hardware_concurrency() * 5) {
   if (!input_data.size())
@@ -17,22 +19,21 @@ auto generate_offset_blocks(
   offsets.allocate(input_data.size() + 1);
   offsets[0] = 0;
   std::size_t current_i = 1;
-  auto task_f =
-      [fill_block_f](auto &&range,
-                     std::pair<tf::buffer<Index>, tf::buffer<T>> &pair) {
-        auto &[sizes, data] = pair;
-        sizes.allocate(range.size());
-        data.reserve(data.size() * 5);
-        auto it = sizes.begin();
-        for (const auto &element : range) {
-          fill_block_f(element, data);
-          *it++ = data.size();
-        }
-      };
+  auto task_f = [fill_block_f](auto &&range,
+                               std::pair<tf::buffer<Index>, Buffer> &pair) {
+    auto &[sizes, data] = pair;
+    sizes.allocate(range.size());
+    data.reserve(data.size() * 5);
+    auto it = sizes.begin();
+    for (const auto &element : range) {
+      auto old_size = data.size();
+      fill_block_f(element, data);
+      *it++ = data.size() - old_size;
+    }
+  };
   auto aggregate_f =
-      [&current_i](
-          const std::pair<tf::buffer<Index>, tf::buffer<T>> &local_result,
-          std::tuple<tf::buffer<Index> &, tf::buffer<T> &> &result) {
+      [&](const std::pair<tf::buffer<Index>, Buffer> &local_result,
+                   std::tuple<tf::buffer<Index> &, Buffer &> &result) {
         auto &[l_sizes, l_data] = local_result;
         auto &[offsets, data] = result;
         auto old_data_size = data.size();
@@ -43,10 +44,28 @@ auto generate_offset_blocks(
           current_i++;
         }
       };
-  tf::blocked_reduce_sequenced_aggregate(
-      input_data, std::tie(offsets, data),
-      std::pair<tf::buffer<Index>, tf::buffer<T>>{}, task_f, aggregate_f,
-      n_tasks);
+  tf::blocked_reduce_sequenced_aggregate(input_data, std::tie(offsets, data),
+                                         std::pair<tf::buffer<Index>, Buffer>{},
+                                         task_f, aggregate_f, n_tasks);
+}
+} // namespace core
+
+template <typename Range, typename Index, typename T, typename F>
+auto generate_offset_blocks(
+    const Range &input_data, tf::buffer<Index> &offsets, tf::buffer<T> &data,
+    const F &fill_block_f,
+    std::size_t n_tasks = std::thread::hardware_concurrency() * 5) {
+  return core::generate_offset_blocks(input_data, offsets, data, fill_block_f,
+                                      n_tasks);
+}
+
+template <typename Range, typename Index, typename T, std::size_t N, typename F>
+auto generate_offset_blocks(
+    const Range &input_data, tf::buffer<Index> &offsets,
+    tf::blocked_buffer<T, N> &data, const F &fill_block_f,
+    std::size_t n_tasks = std::thread::hardware_concurrency() * 5) {
+  return core::generate_offset_blocks(input_data, offsets, data, fill_block_f,
+                                      n_tasks);
 }
 
 template <typename Range, typename Index, typename T, typename F>
