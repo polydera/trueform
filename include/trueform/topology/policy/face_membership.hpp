901942b325a5471a5946976f227f0bcf1def11df
Žiga <ziga.sajovic@gmail.com>
2025-11-05 17:01:14 +0100

    license

--- include/trueform/topology/policy/face_membership.hpp
+++ include/trueform/topology/policy/face_membership.hpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2025 Žiga Sajovic, XLAB
- * Distributed under the Boost Software License, Version 1.0.
+ * Licensed for noncommercial use under the PolyForm Noncommercial License 1.0.0.
+ * Commercial licensing available via ziga.sajovic@xlab.si.
  * https://github.com/xlabmedical/trueform
  */
 
