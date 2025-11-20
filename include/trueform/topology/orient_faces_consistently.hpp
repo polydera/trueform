3428f4b2454ec6ecf2b8e16348da78c12d4e029d
Žiga <ziga.sajovic@gmail.com>
2025-11-20 09:29:54 +0100

    orient_faces_consistently now keeps largest area

--- include/trueform/topology/orient_faces_consistently.hpp
+++ include/trueform/topology/orient_faces_consistently.hpp
@@ -5,40 +5,56 @@
  * https://github.com/xlabmedical/trueform
  */
 #pragma once
+#include "../core/algorithm/parallel_apply.hpp"
 #include "../core/algorithm/parallel_fill.hpp"
+#include "../core/area.hpp"
 #include "../core/buffer.hpp"
 #include "../core/faces.hpp"
+#include "../core/polygons.hpp"
+#include "../core/views/constant.hpp"
+#include "../core/views/indirect_range.hpp"
 #include "./directed_edge_id_in_face.hpp"
+#include "./face_membership.hpp"
 #include "./manifold_edge_link.hpp"
-namespace tf {
+#include "./policy/manifold_edge_link.hpp"
 
-template <typename Policy, typename Policy1>
-void orient_faces_consistently(
-    tf::faces<Policy> &faces,
-    const tf::manifold_edge_link_like<Policy1> &link) {
+namespace tf {
+namespace topology {
+template <typename Policy, typename Policy1, typename Range>
+void orient_faces_consistently(tf::faces<Policy> &faces,
+                               const tf::manifold_edge_link_like<Policy1> &link,
+                               const Range &weights) {
   using Index = std::decay_t<decltype(link[0][0].face_peer)>;
+  using Weight = std::decay_t<decltype(weights[0])>;
+
   tf::buffer<bool> visited;
   visited.allocate(faces.size());
   tf::parallel_fill(visited, false);
 
-  tf::buffer<Index> stack;
-  stack.reserve(64);
+  tf::buffer<Index> queue;
+  queue.reserve(64);
 
   for (Index seed = 0; seed < static_cast<Index>(faces.size()); ++seed) {
     if (visited[seed])
       continue;
 
+    // Start new region
     visited[seed] = true;
-    stack.push_back(seed);
+    queue.clear();
+    queue.push_back(seed);
 
-    while (!stack.empty()) {
-      Index curr = stack.back();
-      stack.pop_back();
+    Weight weight_flipped = 0;
+    Weight weight_not_flipped = weights[seed]; // seed is not flipped
+
+    std::size_t queue_begin = 0;
+
+    while (queue_begin < queue.size()) {
+      Index curr = queue[queue_begin++];
 
       const auto &curr_face = faces[curr];
       const auto &curr_link = link[curr];
 
-      Index size = link.size();
+      Index size = curr_link.size();
       Index prev = size - 1;
       for (Index i = 0; i < size; prev = i++) {
         const auto &peer = curr_link[prev];
@@ -54,21 +70,65 @@ void orient_faces_consistently(
         Index b = curr_face[i];
 
         // Check direction in neighbor face
-        Index dir = tf::directed_edge_id_in_face(neighbor_face, b, a);
+        Index dir = tf::directed_edge_id_in_face(b, a, neighbor_face);
         if (dir == Index(neighbor_face.size())) {
           // Edge direction reversed → flip neighbor face
           std::reverse(neighbor_face.begin(), neighbor_face.end());
+          weight_flipped += weights[neighbor];
+        } else {
+          weight_not_flipped += weights[neighbor];
         }
         visited[neighbor] = true;
-        stack.push_back(neighbor);
+        queue.push_back(neighbor);
       }
+    }
+
+    // If flipped faces have more weight, flip entire region
+    if (weight_flipped > weight_not_flipped) {
+      tf::parallel_apply(
+          tf::make_indirect_range(queue, faces),
+          [](auto &&face) { std::reverse(face.begin(), face.end()); });
     }
   }
 }
+} // namespace topology
 
-template <typename Policy, typename Index, int N>
-void orient_faces_consistently(tf::faces<Policy> &&faces,
-                               const tf::manifold_edge_link<Index, N> &link) {
-  return orient_faces_consistently(faces, link);
+template <typename Policy, typename Policy1>
+void orient_faces_consistently(
+    tf::faces<Policy> &faces,
+    const tf::manifold_edge_link_like<Policy1> &link) {
+  return topology::orient_faces_consistently(
+      faces, link, tf::make_constant_range(1, faces.size()));
+}
+
+template <typename Policy, typename Policy1>
+void orient_faces_consistently(
+    tf::faces<Policy> &&faces,
+    const tf::manifold_edge_link_like<Policy1> &link) {
+  return topology::orient_faces_consistently(faces, link);
+}
+
+template <typename Policy>
+void orient_faces_consistently(tf::polygons<Policy> &polygons) {
+  auto weights = tf::make_mapped_range(
+      polygons, [](const auto &poly) { return tf::area2(poly); });
+  if constexpr (tf::has_manifold_edge_link_policy<Policy>)
+    return topology::orient_faces_consistently(
+        polygons.faces(), polygons.manifold_edge_link(), weights);
+  else {
+    using Index = std::decay_t<decltype(polygons.faces()[0][0])>;
+    tf::face_membership<Index> fm;
+    fm.build(polygons);
+    tf::manifold_edge_link<Index,
+                           tf::static_size_v<decltype(polygons.faces()[0])>>
+        mel;
+    mel.build(polygons.faces(), fm);
+    return topology::orient_faces_consistently(polygons.faces(), mel, weights);
+  }
+}
+
+template <typename Policy>
+void orient_faces_consistently(tf::polygons<Policy> &&polygons) {
+  return orient_faces_consistently(polygons);
 }
 } // namespace tf
