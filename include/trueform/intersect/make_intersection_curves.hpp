285045166749e41a4278d21ef961bd01bcb2245e
ZigaSajovic <ziga.sajovic@gmail.com>
2026-01-18 14:27:33 +0100

    build when missing in intersect module

--- include/trueform/intersect/make_intersection_curves.hpp
+++ include/trueform/intersect/make_intersection_curves.hpp
@@ -12,6 +12,7 @@
 */
 #pragma once
 #include "../core/curves_buffer.hpp"
+#include "../cut/impl/dispatch.hpp"
 #include "../topology/connect_edges_to_paths.hpp"
 #include "./intersections_between_polygons.hpp"
 #include "./make_intersection_edges.hpp"
@@ -22,35 +23,40 @@ namespace tf {
 /// @brief Extract intersection curves where two meshes intersect.
 ///
 /// Computes the geometric intersection between two polygon meshes and
-/// returns the result as connected curves. Use pipe syntax with @ref tf::tag
-/// to add the required tree policy (@ref tf::tree or @ref tf::mod_tree)
+/// returns the result as connected curves. Accepts plain @ref tf::polygons
+/// or forms with precomputed tree policy (@ref tf::tree or @ref tf::mod_tree)
 /// and topology policies (@ref tf::face_membership and @ref tf::manifold_edge_link).
 ///
-/// @tparam Dims The number of dimensions.
-/// @tparam Policy0 The policy type for the first mesh form.
-/// @tparam Policy1 The policy type for the second mesh form.
-/// @param form0 The first mesh @ref tf::form.
-/// @param form1 The second mesh @ref tf::form.
+/// @tparam Policy0 The policy type for the first mesh.
+/// @tparam Policy1 The policy type for the second mesh.
+/// @param _polygons0 The first mesh @ref tf::polygons (or tagged form).
+/// @param _polygons1 The second mesh @ref tf::polygons (or tagged form).
 /// @return A @ref tf::curves_buffer containing connected intersection curves.
 ///
 /// @see tf::intersections_between_polygons for low-level access.
 template <typename Policy0, typename Policy1>
-auto make_intersection_curves(const tf::polygons<Policy0> &form0,
-                              const tf::polygons<Policy1> &form1) {
-  using Index =
-      std::common_type_t<typename Policy0::index_type, typename Policy1::index_type>;
-  tf::intersections_between_polygons<Index, double,
-                                     tf::coordinate_dims_v<Policy0>>
-      ibp;
-  ibp.build(form0, form1);
-  auto ie = tf::make_intersection_edges(ibp);
-  auto paths = tf::connect_edges_to_paths(tf::make_edges(ie));
-  tf::curves_buffer<Index, tf::coordinate_type<Policy0, Policy1>,
-                    tf::coordinate_dims_v<Policy0>>
-      cb;
-  cb.paths_buffer() = std::move(paths);
-  cb.points_buffer().allocate(ibp.intersection_points().size());
-  tf::parallel_copy(ibp.intersection_points(), cb.points());
-  return cb;
+auto make_intersection_curves(const tf::polygons<Policy0> &_polygons0,
+                              const tf::polygons<Policy1> &_polygons1) {
+  return cut::impl::boolean_dispatch(
+      _polygons0, _polygons1, [](const auto &form0, const auto &form1) {
+        using Index =
+            std::common_type_t<typename std::decay_t<decltype(form0)>::index_type,
+                               typename std::decay_t<decltype(form1)>::index_type>;
+        tf::intersections_between_polygons<Index, double,
+                                           tf::coordinate_dims_v<std::decay_t<decltype(form0)>>>
+            ibp;
+        ibp.build(form0, form1);
+        auto ie = tf::make_intersection_edges(ibp);
+        auto paths = tf::connect_edges_to_paths(tf::make_edges(ie));
+        tf::curves_buffer<Index,
+                          tf::coordinate_type<std::decay_t<decltype(form0)>,
+                                              std::decay_t<decltype(form1)>>,
+                          tf::coordinate_dims_v<std::decay_t<decltype(form0)>>>
+            cb;
+        cb.paths_buffer() = std::move(paths);
+        cb.points_buffer().allocate(ibp.intersection_points().size());
+        tf::parallel_copy(ibp.intersection_points(), cb.points());
+        return cb;
+      });
 }
 } // namespace tf
--- include/trueform/intersect/make_self_intersection_curves.hpp
+++ include/trueform/intersect/make_self_intersection_curves.hpp
@@ -12,9 +12,16 @@
 */
 #pragma once
 #include "../core/curves_buffer.hpp"
+#include "../spatial/aabb_tree.hpp"
+#include "../spatial/policy/tree.hpp"
 #include "../topology/connect_edges_to_paths.hpp"
+#include "../topology/face_membership.hpp"
+#include "../topology/manifold_edge_link.hpp"
+#include "../topology/policy/face_membership.hpp"
+#include "../topology/policy/manifold_edge_link.hpp"
 #include "./intersections_within_polygons.hpp"
 #include "./make_intersection_edges.hpp"
+#include "tbb/parallel_invoke.h"
 
 namespace tf {
 
@@ -23,31 +30,66 @@ namespace tf {
 ///
 /// Finds all locations where a mesh's faces intersect each other
 /// (excluding adjacent faces) and returns the result as connected curves.
-/// Use pipe syntax with @ref tf::tag to add the required tree policy
+/// Accepts plain @ref tf::polygons or forms with precomputed tree policy
 /// (@ref tf::tree or @ref tf::mod_tree) and topology policies
 /// (@ref tf::face_membership and @ref tf::manifold_edge_link).
 ///
-/// @tparam Dims The number of dimensions.
-/// @tparam Policy The policy type for the mesh form.
-/// @param form The mesh @ref tf::form.
+/// @tparam Policy The policy type for the mesh.
+/// @param _polygons The input @ref tf::polygons (or tagged form).
 /// @return A @ref tf::curves_buffer containing connected self-intersection curves.
 ///
 /// @see tf::intersections_within_polygons for low-level access.
 template <typename Policy>
-auto make_self_intersection_curves(const tf::polygons<Policy> &form) {
-  using Index = typename Policy::index_type;
-  tf::intersections_within_polygons<Index, double,
-                                    tf::coordinate_dims_v<Policy>>
-      iwp;
-  iwp.build(form);
-  auto ie = tf::make_intersection_edges(iwp);
-  auto paths = tf::connect_edges_to_paths(tf::make_edges(ie));
-  tf::curves_buffer<Index, tf::coordinate_type<Policy>,
-                    tf::coordinate_dims_v<Policy>>
-      cb;
-  cb.paths_buffer() = std::move(paths);
-  cb.points_buffer().allocate(iwp.intersection_points().size());
-  tf::parallel_copy(iwp.intersection_points(), cb.points());
-  return cb;
+auto make_self_intersection_curves(const tf::polygons<Policy> &_polygons) {
+  if constexpr (!tf::has_tree_policy<Policy> &&
+                !tf::has_manifold_edge_link_policy<Policy>) {
+    using Index = std::decay_t<decltype(_polygons.faces()[0][0])>;
+    tf::aabb_tree<Index, tf::coordinate_type<Policy>,
+                  tf::coordinate_dims_v<Policy>>
+        tree;
+    tf::face_membership<Index> fm;
+    tf::manifold_edge_link<Index,
+                           tf::static_size_v<decltype(_polygons.faces()[0])>>
+        mel;
+    tbb::parallel_invoke([&] { tree.build(_polygons, tf::config_tree(4, 4)); },
+                         [&] {
+                           fm.build(_polygons);
+                           mel.build(_polygons.faces(), fm);
+                         });
+    return make_self_intersection_curves(_polygons | tf::tag(fm) |
+                                         tf::tag(mel) | tf::tag(tree));
+  } else if constexpr (!tf::has_tree_policy<Policy>) {
+    using Index = std::decay_t<decltype(_polygons.faces()[0][0])>;
+    tf::aabb_tree<Index, tf::coordinate_type<Policy>,
+                  tf::coordinate_dims_v<Policy>>
+        tree;
+    tree.build(_polygons, tf::config_tree(4, 4));
+    return make_self_intersection_curves(_polygons | tf::tag(tree));
+  } else if constexpr (!tf::has_manifold_edge_link_policy<Policy>) {
+    using Index = std::decay_t<decltype(_polygons.faces()[0][0])>;
+    tf::face_membership<Index> fm;
+    tf::manifold_edge_link<Index,
+                           tf::static_size_v<decltype(_polygons.faces()[0])>>
+        mel;
+    fm.build(_polygons);
+    mel.build(_polygons.faces(), fm);
+    return make_self_intersection_curves(_polygons | tf::tag(fm) |
+                                         tf::tag(mel));
+  } else {
+    using Index = typename Policy::index_type;
+    tf::intersections_within_polygons<Index, double,
+                                      tf::coordinate_dims_v<Policy>>
+        iwp;
+    iwp.build(_polygons);
+    auto ie = tf::make_intersection_edges(iwp);
+    auto paths = tf::connect_edges_to_paths(tf::make_edges(ie));
+    tf::curves_buffer<Index, tf::coordinate_type<Policy>,
+                      tf::coordinate_dims_v<Policy>>
+        cb;
+    cb.paths_buffer() = std::move(paths);
+    cb.points_buffer().allocate(iwp.intersection_points().size());
+    tf::parallel_copy(iwp.intersection_points(), cb.points());
+    return cb;
+  }
 }
 } // namespace tf
